2d62a9799526174fcada3611b5dee7847ab593d7
Mohamed Langi <mlangi093@gmail.com>
2019-10-09 17:19:14 -0500

    JS code

---
+++ week-8/week-8.js
@@ -0,0 +1,19 @@
+alert("HELLO WORLD")
+console.log("Basic Log");
+console.error("ERROR LOG")
+console.warn("WARNING LOG")
+
+let x = 5;
+let y = 3;
+let z = x + y;
+
+console.log(z)
+
+z = 20;
+console.log(z)
+
+z = "This is a text"
+console.log(z)
+
+
+
