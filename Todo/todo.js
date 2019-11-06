3119fcc13b5a1cfef14022a8d89014dfcece6945
Mohamed Langi <mlangi093@gmail.com>
2019-11-06 17:52:12 -0600

    added localstorage

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,6 +1,8 @@
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
+
+let todos = [];
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -9,50 +11,87 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-
 // Click on a close button to hide the current list item
 let close = document.getElementsByClassName("close");
 for (let i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
 // Add a "checked" symbol when clicking on a list item
-let list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
+let list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function(ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
 
 function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("todoInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("todoUL").appendChild(li);
+  }
+  document.getElementById("todoInput").value = "";
+
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+  saveTolocalStorage(inputValue);
+}
+
+populateTodo = () => {
+  for (todo of todos) {
+    
     var li = document.createElement("li");
-    var inputValue = document.getElementById("todoInput").value;
-    var t = document.createTextNode(inputValue);
+    var t = document.createTextNode(todo);
     li.appendChild(t);
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("todoUL").appendChild(li);
-    }
-    document.getElementById("todoInput").value = "";
-  
+    document.getElementById("todoUL").appendChild(li);
+
     let span = document.createElement("SPAN");
     let txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
 
-    
-    for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-          var div = this.parentElement;
-          div.style.display = "none";
-        }
-      }
-  
+    console.log(li)
 
+    for (let i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      };
+    }
   }
+};
+
+saveTolocalStorage = todo => {
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+window.onload = () => {
+  if (localStorage.getItem("todos") !== null) {
+    todos = JSON.parse(localStorage.getItem("todos"));
+    populateTodo();
+  }
+};
